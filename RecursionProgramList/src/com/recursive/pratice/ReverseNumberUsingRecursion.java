bda580af4c87381c47c06b2d5d013ef12b9076fb
johnM66 <32862342+johnM66@users.noreply.github.com>
2022-03-24 22:31:12 +0530

    commit

---
+++ RecursionProgramList/src/com/recursive/pratice/ReverseNumberUsingRecursion.java
@@ -0,0 +1,24 @@
+package com.recursive.pratice;
+
+public class ReverseNumberUsingRecursion {
+
+	public static void main(String[] args) {
+		int number = 5698;
+		//reverse number is 8965
+		reverseNumber(number);
+
+	}
+
+	private static int reverseNumber(int number) {
+		//base case
+		if(number<=0)
+			
+		return number;
+		// TODO Auto-generated method stub
+		return number;
+		
+	}
+
+	
+
+}
