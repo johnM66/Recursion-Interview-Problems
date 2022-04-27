c5f84812f47dee1403750d99190cb9f09d8bad71
johnM66 <32862342+johnM66@users.noreply.github.com>
2022-04-27 20:43:09 +0530

    case change

---
+++ RecursionProgramList/src/com/recursive/pratice/PermutationWithCaseChange.java
@@ -0,0 +1,22 @@
+package com.recursive.pratice;
+
+public class PermutationWithCaseChange {
+
+	public static void main(String[] args) {
+		String input="ab";
+		String output="";
+				
+		solve(input,output);
+
+	}
+
+	private static void solve(String input, String output) {
+		if(input.length()==0) {
+			System.out.println(output);
+		return;
+	}
+		//taking decision if need to take the value or not
+		solve(input.substring(1), output+String.valueOf(input.charAt(0)).toUpperCase());
+		solve(input.substring(1),output+input.charAt(0));
+	}
+}
